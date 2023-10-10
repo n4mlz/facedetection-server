28ddba42cc4ecc5bc0f47af4bdc6808a62932a41
Takaharu Nakamura <64473501+appare45@users.noreply.github.com>
2023-10-10 21:24:14 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactNode, useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Row: React.FC<{
+  children: ReactNode;
+  data: timeData;
+}> = (props) => {
+  const columns = [];
+  for (let i = 0; i < Object.keys(props.data).length; i++) {
+    columns.push(<td key={i} />);
+  }
+  return (
+    <tr>
+      <td>{props.children}</td>
+      {columns}
+    </tr>
+  );
+};
 
+const Table: React.FC<{
+  data: timeData;
+}> = (props) => {
+  const times = [];
+  for (let i = 0; i < 24; i++) {
+    times.push(<Row key={i} data={props.data}>{`${i + 1}時`}</Row>);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {Object.keys(props.data).map((person) => (
+        <>
+          {props.data[person].map((e) => (
+            <div style={{ top: `${(new Date(e)).getHours() * 60 + (new Date(e)).getMinutes()}px` }} >{(new Date(e)).toLocaleString() + e}</div>
+            
+          ))}
+        </>
+      ))}
+      <table className="time">
+        <tr className="tableHead">
+          <th className="table"></th>
+          {Object.keys(props.data).map((e) => (
+            <th key={e}>{e}</th>
+          ))}
+        </tr>
+        <tbody>{times}</tbody>
+      </table>
     </>
-  )
+  );
+};
+type timeData = {
+  [key: string]: string[];
+};
+
+function dateToString(date: Date) {
+  return `${date.getFullYear()}-${("00" + (date.getMonth() + 1)).slice(-2)}-${(
+    "00" + date.getDate()
+  ).slice(-2)}`;
 }
 
-export default App
+function App() {
+  const [date, setDate] = useState(new Date());
+  const [data, setData] = useState<timeData>({});
+  useEffect(() => {
+    setData({});
+    fetch(`/${dateToString(date)}.json`).then((res) => {
+      res.text().then((e) => {
+        setData(JSON.parse(e));
+      });
+    });
+  }, [date]);
+  return (
+    <div className="table">
+      <input
+        type="date"
+        name=""
+        value={dateToString(date)}
+        id="timeSelector"
+        onChange={(e) => {
+          setDate(new Date(e.target.value));
+        }}
+      />
+      {!Object.keys(data).length ? (
+        <p>データがありません</p>
+      ) : (
+        <Table data={data} />
+      )}
+    </div>
+  );
+}
+
+export default App;
